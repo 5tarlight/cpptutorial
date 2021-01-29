ffc8cf12c20bf3d572ce560310680b92cd9f4c96
5tarlight <developerstarlight@gmail.com>
2021-01-29 23:31:14 +0900

    ww

---
+++ func/FuncTemplate.h
@@ -0,0 +1,13 @@
+//
+// Created by devel on 2021-01-17.
+//
+
+#ifndef CPPTUTORIAL_FUNCTEMPLATE_H
+#define CPPTUTORIAL_FUNCTEMPLATE_H
+
+template<typename T>
+T add(T a, T b) {
+    return a + b;
+}
+
+#endif //CPPTUTORIAL_FUNCTEMPLATE_H
